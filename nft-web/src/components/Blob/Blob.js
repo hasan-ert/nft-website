c84bd4538dc71b7d6d3faed17017f09cbd0605b7
hasan-ert <68220593+hasan-ert@users.noreply.github.com>
2023-05-21 01:08:47 +0300

    create blob element

---
+++ nft-web/src/components/Blob/Blob.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Css
+import { Canvas } from "react-three-fiber";
+import { Sphere, MeshDistortMaterial } from "@react-three/drei";
+export default function Blob() {
+    return (
+        <Canvas style={{ height: "300px", width: "300px" }}>
+            <ambientLight intensity={0.5}></ambientLight>
+            <spotLight position={[10, 10, 10]} angle={0.95}></spotLight>
+            <Sphere args={[1, 100, 100]} scale={[3, 3, 1]} color={"red"}>
+                <MeshDistortMaterial
+                    speed={2}
+                    distort={0.5}
+                    color="#6d1a99"
+                    roughness={0}
+                ></MeshDistortMaterial>
+            </Sphere>
+        </Canvas>
+    );
+}
